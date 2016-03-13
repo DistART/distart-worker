d29a823fe9615ea2c08fb8aa9d2d946da06d9deb
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2016-03-13 10:35:15 +0100

    fix !

--- src/child-process.sh
+++ src/child-process.sh
@@ -7,7 +7,7 @@ out_path="$3"
 image_size="$4"
 num_iteractions="$5"
 
-cd "NEURAL_STYLE_HOME"
+cd "$NEURAL_STYLE_HOME"
 
 deepart_command="th neural_style.lua -image_size \"$image_size\" -num_iterations \"$num_iteractions\" -style_image \"$pattern_path\" -content_image \"$image_path\" -backend cudnn -output_image \"$out_path\""
 
